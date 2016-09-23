a97a5d22a70d4a2ac9ff1366e78f914550dcd78e
Eliezer Wohl <eliezerwohl@gmail.com>
2016-09-23 11:34:25 -0400

    seperated header to it's own php files

--- index.php
+++ index.php
@@ -1,22 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://bootswatch.com/readable/bootstrap.min.css">
-<link href='https://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" type="text/css" href="assets/styles/styles.css">
-  <title>Liberty Vice</title>
-</head>
-<?php include("templates/topNav.php") ?>
+<?php include("templates/header.php"); ?>
+<?php include("templates/topNav.php"); ?>
 <div class="col-md-12 container-fluid">
 
-<?php include("templates/home.php") ?>
-<?php include("templates/bandHistory.php") ?>
-<?php include("templates/demoSection.php") ?>
-<?php include("templates/memberSection.php") ?>
-<?php include("templates/contactUs.php") ?>
+<?php include("templates/home.php"); ?>
+<?php include("templates/bandHistory.php"); ?>
+<?php include("templates/demoSection.php"); ?>
+<?php include("templates/memberSection.php"); ?>
+<?php include("templates/contactUs.php"); ?>
 </div>
 
 
