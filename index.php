95cf799717f73ca824ef8891e2f8196197640bfd
Eliezer Wohl <eliezerwohl@gmail.com>
2016-09-23 11:31:03 -0400

    seperated top nav off index.php to it's own template

--- index.php
+++ index.php
@@ -9,28 +9,7 @@
 <link rel="stylesheet" type="text/css" href="assets/styles/styles.css">
   <title>Liberty Vice</title>
 </head>
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" id="navLogo" href="<?php echo $_SERVER['PHP_SELF'] ?>">Liberty Vice</a>
-    </div>
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-    <ul class="nav navbar-nav navbar-right">
-      <li data-id="bandHistorySection"><a data-toggle="pill" href="#">Band History</a></li>
-      <li data-id="membersSection"><a  data-toggle="pill" href="#">Members</a></li>
-      <li data-id="contactUsSection"><a data-toggle="pill" href="#">Contact Us</a></li>
-      <li data-toggle="pill" data-id="demosSection"><a>Demos</a></li>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+<?php include("templates/topNav.php") ?>
 <div class="col-md-12 container-fluid">
 
 <?php include("templates/home.php") ?>
